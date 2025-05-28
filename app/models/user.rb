d36c391df09d1371e9aa8d0227012bd328df8f0e
Nga Man Yeung(Grace) <graceyeungnm@gmail.com>
2025-05-28 12:09:49 +0100

    fixed spelling mistake on uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :user, uniqueness: true
-  validates :email, uniquesness: true
+  validates :email, uniqueness: true
 
   has_many :meals, dependent: :destroy
 end
