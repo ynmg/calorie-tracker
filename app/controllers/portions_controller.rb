5d2f201097f731e0bd9e3a41cc29a78bf419197d
Alex Gartland <96433928+AGartland22@users.noreply.github.com>
2025-05-29 17:45:22 +0100

    Revert "Added delete method for portions controller, updated meals index view…"

--- app/controllers/portions_controller.rb
+++ app/controllers/portions_controller.rb
@@ -1,11 +1,4 @@
 class PortionsController < ApplicationController
-  
-  def destroy
-    @portion = Portion.find(params[:id])
-    @portion.destroy
-    redirect_to meals_path
-  end
-
   before_action :set_portion, only: [ :show, :update ]
 
   def show
