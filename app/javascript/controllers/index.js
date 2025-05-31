d69b4f241b0e4d6b2cd977ee3ba23395ef3bfcf2
Nga Man Yeung(Grace) <graceyeungnm@gmail.com>
2025-05-31 12:50:15 +0100

    Added stimulus controller counter, added countup.js, added countup effect on total calories on meals/index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+// register countercontroller
+import CounterController from "./counter_controller"
+application.register("counter", CounterController)
